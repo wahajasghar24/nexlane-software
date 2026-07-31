f67f2d7cb09ef1af01e9024ae007720f1d81ba47
root <root@srv1766087.hstgr.cloud>
2026-07-31 13:09:49 +0000

    chore: debug env route

---
+++ src/app/api/debug-env/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.json({
+    supabaseUrl: !!process.env.NEXT_PUBLIC_SUPABASE_URL,
+    anonKey: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    serviceRole: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+    nodeEnv: process.env.NODE_ENV,
+  })
+}
