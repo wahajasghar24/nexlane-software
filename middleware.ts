9adf6a21d939c813037a096a8f47d51523935191
root <root@srv1766087.hstgr.cloud>
2026-08-06 15:42:52 +0000

    chore(i18n): wrap middleware for debug header (confirm next-intl matcher runs)

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import createMiddleware from 'next-intl/middleware'
+import type { NextRequest } from 'next/server'
 import { routing } from './src/i18n/routing'
 
-export default createMiddleware(routing)
+export default async function middleware(request: NextRequest) {
+  const res = await createMiddleware(routing)(request)
+  res.headers.set('x-next-intl-matcher', 'RAN')
+  return res
+}
 
 export const config = {
   // Match all paths except api, _next, static files
